a8a1f1f68e0f26223a40fb61e16fbca1bb4475e9
CJ Salem <cj.salem@fishtech.group>
2019-02-26 21:23:57 -0600

    test exports

--- index.test.js
+++ index.test.js
@@ -1,8 +1,19 @@
+/* eslint-disable no-shadow,no-sync */
+
+const process = require('child_process');
+
 describe('index.js', () => {
-    test('index.js exports correctly', () => {
-        const index = require('.');
-        expect(typeof index).toBe('object');
-        expect(index).toHaveProperty('default');
-        expect(typeof index.default).toBe('function');
+    const k = require('.');
+    test('exports expected functions and classes', () => {
+        expect(k).toHaveProperty('PipeConsumer', expect.any(Function));
+        expect(k).toHaveProperty('createConsumer', expect.any(Function));
+        expect(k).toHaveProperty('createProducer', expect.any(Function));
+        expect(k).toHaveProperty('createTransformer', expect.any(Function));
+    });
+    test('dist is up to date', () => {
+        const oldHash = process.execSync('cat dist/* | md5').toString();
+        process.execSync('npm run build');
+        const newHash = process.execSync('cat dist/* | md5').toString();
+        expect(newHash).toBe(oldHash);
     });
 });
