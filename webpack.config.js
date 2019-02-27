c29d80b7776449d207f047efc99d1e13e400f2b7
CJ Salem <cj.salem@fishtech.group>
2019-02-27 13:02:34 -0600

    export mocks

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,42 @@
 const path = require('path');
 
-module.exports = {
-    entry: './src/kafka-pipe.js',
-    target: 'node',
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                include: __dirname,
-                exclude: /node_modules/,
-            },
-        ],
+module.exports = [
+    {
+        entry: './src/kafka-pipe.js',
+        target: 'node',
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    loader: 'babel-loader',
+                    include: __dirname,
+                    exclude: /node_modules/,
+                },
+            ],
+        },
+        output: {
+            libraryTarget: 'commonjs',
+            path: path.join(__dirname, 'dist'),
+            filename: 'index.js',
+        },
     },
-    output: {
-        libraryTarget: 'commonjs',
-        path: path.join(__dirname, 'dist'),
-        filename: 'index.js',
+    {
+        entry: './__mocks__/kafka-node.js',
+        target: 'node',
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    loader: 'babel-loader',
+                    include: __dirname,
+                    exclude: /node_modules/,
+                },
+            ],
+        },
+        output: {
+            libraryTarget: 'commonjs',
+            path: path.join(__dirname, 'dist/__mocks__'),
+            filename: 'index.js',
+        },
     },
-};
+];
