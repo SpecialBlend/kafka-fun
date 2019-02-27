7efb0e5496092056a7c99e49e854ea5d0047a3c0
CJ Salem <cj.salem@fishtech.group>
2019-02-26 21:50:39 -0600

    remove system incompatible test

--- index.test.js
+++ index.test.js
@@ -1,7 +1,5 @@
 /* eslint-disable no-shadow,no-sync */
 
-const process = require('child_process');
-
 describe('index.js', () => {
     const k = require('.');
     test('exports expected functions and classes', () => {
@@ -9,11 +7,5 @@ describe('index.js', () => {
         expect(k).toHaveProperty('createConsumer', expect.any(Function));
         expect(k).toHaveProperty('createProducer', expect.any(Function));
         expect(k).toHaveProperty('createTransformer', expect.any(Function));
-    });
-    test('dist is up to date', () => {
-        const oldHash = process.execSync('cat dist/* | md5').toString();
-        process.execSync('npm run build');
-        const newHash = process.execSync('cat dist/* | md5').toString();
-        expect(newHash).toBe(oldHash);
     });
 });
