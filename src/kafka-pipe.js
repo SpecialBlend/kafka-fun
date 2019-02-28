5efdca23c563c9b294618f7f2268581b9aa023d5
CJ <cj.salem@fishtech.group>
2019-02-28 14:54:42 -0600

    curry exported functions

--- src/kafka-pipe.js
+++ src/kafka-pipe.js
@@ -1,5 +1,5 @@
 /* eslint-disable max-params */
-
+import * as R from 'ramda';
 import { Consumer, Producer } from 'kafka-node';
 import { promisify } from 'util';
 
@@ -27,43 +27,49 @@ export class PipeProducer extends Producer {
 
 /**
  * Create Piped Consumer
- * @param {KafkaClient} client: kafka client
+ * @param {Client} client: kafka client
  * @param {String} topic: name of topic
  * @param {Object|none} topicSettings: optional topic settings
  * @param {Object|none} options: optional consumer settings
  * @return {PipeConsumer}: PipeConsumer
  */
-export const createConsumer = (client, topic, topicSettings = {}, options = {}) =>
-    new PipeConsumer(client, [{ ...topicSettings, topic }], options);
+export const createConsumer = R.curry(
+    (client, topic, topicSettings = {}, options = {}) =>
+        new PipeConsumer(client, [{ ...topicSettings, topic }], options)
+);
 
 /**
  * Create curried Producer.send function
- * @param {KafkaClient} client: kafka client
+ * @param {Client} client: kafka client
  * @param {String} topic: name of topic
  * @param {Object|null} sendSettings: optional send settings
  * @param {Object|null} options: optional producer settings
  * @return {Function}: The resulting curried send function
  */
-export const createSender = (client, topic, sendSettings = {}, options = {}) => {
-    const producer = new PipeProducer(client, options);
-    return messages => producer.send([{
-        ...sendSettings,
-        topic,
-        messages,
-    }]);
-};
+export const createSender = R.curry(
+    (client, topic, sendSettings = {}, options = {}) => {
+        const producer = new PipeProducer(client, options);
+        return messages => producer.send([{
+            ...sendSettings,
+            topic,
+            messages,
+        }]);
+    }
+);
 
 /**
  * Create Transformer that reads from sourceTopic, transforms data then pipes to destinationTopic
- * @param {KafkaClient} client: kafka client
+ * @param {Client} client: kafka client
  * @param {String} sourceTopic: source kafka topic name
  * @param {String} destinationTopic: destination kafka topic name
  * @param {Function} transform: transformer function
  * @return {PipeConsumer}: PipeConsumer
  */
-export const createTransformer = (client, sourceTopic, destinationTopic, transform) => {
-    const sendToDestination = createSender(client, destinationTopic);
-    const consumer = createConsumer(client, sourceTopic);
-    consumer.pipe(message => sendToDestination([transform(message)]));
-    return consumer;
-};
+export const createTransformer = R.curry(
+    (client, sourceTopic, destinationTopic, transform) => {
+        const sendToDestination = createSender(client, destinationTopic);
+        const consumer = createConsumer(client, sourceTopic);
+        consumer.pipe(message => sendToDestination([transform(message)]));
+        return consumer;
+    }
+);
--- src/kafka-pipe.test.js
+++ src/kafka-pipe.test.js
@@ -32,6 +32,13 @@ describe('createConsumer', () => {
         expect(callback).toHaveBeenCalledWith('test.message.0');
         expect(callback).toHaveBeenCalledWith('test.message.1');
     });
+    test('is properly curried', () => {
+        expect(typeof createConsumer(client)).toBe('function');
+        const testConsumer = createConsumer(client, topic, {}, {});
+        expect(createConsumer(client)(topic)).toMatchObject(testConsumer);
+        expect(createConsumer(client)(topic, {})).toMatchObject(testConsumer);
+        expect(createConsumer(client)(topic, {}, {})).toMatchObject(testConsumer);
+    });
 });
 
 describe('createSender', () => {
@@ -46,16 +53,16 @@ describe('createSender', () => {
     const optionalProducerSettings = {
         ackTimeoutMs: 500,
     };
-    const sendToTopic = createSender(client, topic, optionalSendSettings, optionalProducerSettings);
+    const sendMessage = createSender(client, topic, optionalSendSettings, optionalProducerSettings);
     test('returns instance of Function', () => {
-        expect(typeof sendToTopic).toBe('function');
+        expect(typeof sendMessage).toBe('function');
     });
     test('calls Producer constructor with expected data', () => {
         expect(__kafkaProducerConstruct).toHaveBeenCalledWith(client, optionalProducerSettings);
     });
     test('resolver calls Producer.send with expected data', async() => {
         __kafkaProducerSend.mockResolvedValueOnce(response);
-        const sendResponse = await sendToTopic(messages);
+        const sendResponse = await sendMessage(messages);
         expect(__kafkaProducerSend).toHaveBeenCalledWith([{
             key,
             topic,
@@ -63,27 +70,49 @@ describe('createSender', () => {
         }]);
         expect(sendResponse).toBe(response);
     });
+    test('is properly curried', async() => {
+        const expectedParams = [{ messages, topic }];
+        __kafkaProducerSend.mockReset();
+        await createSender(client)(topic)(messages);
+        expect(__kafkaProducerSend).toHaveBeenCalledWith(expectedParams);
+        __kafkaProducerSend.mockReset();
+        await createSender(client)(topic, {})(messages);
+        expect(__kafkaProducerSend).toHaveBeenCalledWith(expectedParams);
+        __kafkaProducerSend.mockReset();
+        await createSender(client)(topic, {}, {})(messages);
+        expect(__kafkaProducerSend).toHaveBeenCalledWith(expectedParams);
+    });
 });
 
-test('createTransformer works as expected', () => {
+describe('createTransformer', () => {
     __kafkaConsumerConstruct.mockReset();
     __kafkaProducerConstruct.mockReset();
     __kafkaProducerSend.mockReset();
     const client = new KafkaClient();
     const sourceTopic = 'test.source.topic';
     const destinationTopic = 'test.destination.topic';
-    const transformer = jest.fn(R.reverse);
-    const consumer = createTransformer(client, sourceTopic, destinationTopic, transformer);
+    const transform = jest.fn(R.reverse);
+    const transformer = createTransformer(client, sourceTopic, destinationTopic, transform);
     const message = 'test.message';
     const response = 'test.response';
-    expect(consumer).toBeInstanceOf(PipeConsumer);
-    expect(__kafkaProducerConstruct).toHaveBeenCalledWith(client, {});
-    expect(__kafkaConsumerConstruct).toHaveBeenCalledWith(client, [{ topic: sourceTopic }], {});
-    __kafkaProducerSend.mockResolvedValueOnce(response);
-    consumer.emit('message', message);
-    expect(transformer).toHaveBeenCalledWith(message);
-    expect(__kafkaProducerSend).toHaveBeenCalledWith([{
-        topic: destinationTopic,
-        messages: [transformer(message)],
-    }]);
+    test('returns instance of PipeConsumer', () => {
+        expect(transformer).toBeInstanceOf(PipeConsumer);
+        expect(__kafkaProducerConstruct).toHaveBeenCalledWith(client, {});
+        expect(__kafkaConsumerConstruct).toHaveBeenCalledWith(client, [{ topic: sourceTopic }], {});
+    });
+    test('calls Producer.send with expected data', () => {
+        __kafkaProducerSend.mockResolvedValueOnce(response);
+        transformer.emit('message', message);
+        expect(transform).toHaveBeenCalledWith(message);
+        expect(__kafkaProducerSend).toHaveBeenCalledWith([{
+            topic: destinationTopic,
+            messages: [transform(message)],
+        }]);
+    });
+    test('is properly curried', () => {
+        expect(typeof createTransformer(client)).toBe('function');
+        expect(typeof createTransformer(client, sourceTopic)).toBe('function');
+        expect(typeof createTransformer(client, sourceTopic, destinationTopic)).toBe('function');
+        expect(createTransformer(client)(sourceTopic)(destinationTopic)(transform)).toBeInstanceOf(PipeConsumer);
+    });
 });
