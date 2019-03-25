52f0f1d79734f2744a97be4ed7188e6559827cca
specialblend <cj@specialblend.net>
2019-03-25 00:44:19 -0500

    test package exports

--- exports.test.js
+++ exports.test.js
@@ -1,9 +1,9 @@
-import moduleES from './dist/index.esm';
-import moduleCJS from './dist/index.cjs';
+import * as moduleES from './dist/index.esm';
+import * as moduleCJS from './dist/index.cjs';
 const moduleUMD = require('./dist/index.umd');
 
-test('exports are importable', () => {
-    expect(moduleUMD).toBeFunction();
-    expect(moduleES).toBeFunction();
-    expect(moduleCJS).toBeFunction();
+test('exports correctly', () => {
+    expect(moduleES).toHaveProperty('PipeConsumer', expect.any(Function));
+    expect(moduleCJS).toHaveProperty('PipeConsumer', expect.any(Function));
+    expect(moduleUMD).toHaveProperty('PipeConsumer', expect.any(Function));
 });
